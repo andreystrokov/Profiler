92e2338f79e3186441d2b4428b353c3c39ee7388
Andrey <68009565+andreystrokov@users.noreply.github.com>
2021-07-22 13:28:00 +0300

    Detele excess symbols

--- Log_duration.h
+++ Log_duration.h
@@ -29,6 +29,6 @@ private:
 };
 #define  UNIQ_ID_IMPL(lineno) _a_local_var_##lineno
 #define UNIQ_ID(lineno) UNIQ_ID_IMPL(lineno)
-#define LOG_DURATION (message){   \
-LogDuration UNIQ_ID(__LINE__){message};           \
-}
+#define LOG_DURATION (message)   \
+LogDuration UNIQ_ID(__LINE__){message};
+
